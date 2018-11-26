2bc473b1209bf1bed59fd048bd78e02292efc529
Anthony Duzhenko <duha.06@ukr.net>
2018-11-26 18:33:59 +0200

    update git ignore

--- app/src/main/java/com/example/aduzhenko/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/aduzhenko/mediaplayer/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.aduzhenko.mediaplayer;
 
-import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
